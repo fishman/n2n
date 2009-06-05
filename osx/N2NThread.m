87d12903a588d5814fecab0818981431412417d1
Reza Jelveh <toyb0mb@gmx.net>
2009-06-05 20:30:18 +0200

    n2n thread object added

---
+++ osx/N2NThread.m
@@ -0,0 +1,15 @@
+//
+//  N2NThread.m
+//  n2n
+//
+//  Created by Reza Jelveh on 6/5/09.
+//  Copyright 2009 Protonet.info. All rights reserved.
+//
+
+#import "N2NThread.h"
+
+@implementation N2NThread
+
+
+
+@end
