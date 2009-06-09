e8244ce8c74c0db7253fa332ef9d25ed11aba593
Reza Jelveh <toyb0mb@gmx.net>
2009-06-10 00:52:53 +0200

    fix copyright

--- osx/main.m
+++ osx/main.m
@@ -3,7 +3,7 @@
 //  n2n
 //
 //  Created by Reza Jelveh on 6/5/09.
-//  Copyright Acculogic GmbH 2009. All rights reserved.
+//  Copyright Protonet.info 2009. All rights reserved.
 //
 
 #import <Cocoa/Cocoa.h>
