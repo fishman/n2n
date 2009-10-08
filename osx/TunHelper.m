02fb9c54ba782fc3fd6d863e4a9ee917fd5f97b6
Reza Jelveh <toyb0mb@gmx.net>
2009-10-08 21:19:39 +0200

    change the send_fd function

--- osx/TunHelper.m
+++ osx/TunHelper.m
@@ -11,49 +11,36 @@
 #include <sys/un.h>
 #include <limits.h>
 
-#include "ancillary.h"
-
 #define ADDRESS "/tmp/somesocketstuffs"
 #define N2N_OSX_TAPDEVICE_SIZE 32
 
-/* size of control buffer to send/recv one file descriptor */
-#define CONTROLLEN  CMSG_LEN(sizeof(int))
-
-static struct cmsghdr   *cmptr = NULL;  /* malloc'ed first time */
-
-int send_fd(int fd, int fd_to_send)
+int send_fd(int sock, int fd)
 {
-    struct iovec    iov[1];
-    struct msghdr   msg;
-    char            buf[2]; /* send_fd()/recv_fd() 2-byte protocol */
+    struct msghdr msghdr;
+    char nothing = '!';
+    struct iovec nothing_ptr;
+    struct cmsghdr *cmsg;
+    struct {
+        struct cmsghdr h;
+        int fd[1];
+    } buffer;
 
-    iov[0].iov_base = buf;
-    iov[0].iov_len  = 2;
-    msg.msg_iov     = iov;
-    msg.msg_iovlen  = 1;
-    msg.msg_name    = NULL;
-    msg.msg_namelen = 0;
-    if (fd_to_send < 0) {
-        msg.msg_control    = NULL;
-        msg.msg_controllen = 0;
-        buf[1] = -fd_to_send;   /* nonzero status means error */
-        if (buf[1] == 0)
-            buf[1] = 1; /* -256, etc. would screw up protocol */
-    } else {
-        if (cmptr == NULL && (cmptr = malloc(CONTROLLEN)) == NULL)
-            return(-1);
-        cmptr->cmsg_level  = SOL_SOCKET;
-        cmptr->cmsg_type   = SCM_RIGHTS;
-        cmptr->cmsg_len    = CONTROLLEN;
-        msg.msg_control    = cmptr;
-        msg.msg_controllen = CONTROLLEN;
-        *(int *)CMSG_DATA(cmptr) = fd_to_send;     /* the fd to pass */
-        buf[1] = 0;          /* zero status means OK */
-    }
-    buf[0] = 0;              /* null byte flag to recv_fd() */
-    if (sendmsg(fd, &msg, 0) != 2)
-        return(-1);
-    return(0);
+    nothing_ptr.iov_base = &nothing;
+    nothing_ptr.iov_len = 1;
+    msghdr.msg_name = NULL;
+    msghdr.msg_namelen = 0;
+    msghdr.msg_iov = &nothing_ptr;
+    msghdr.msg_iovlen = 1;
+    msghdr.msg_flags = 0;
+    msghdr.msg_control = (void*)&buffer;
+    msghdr.msg_controllen = sizeof(struct cmsghdr) + sizeof(int);
+    cmsg = CMSG_FIRSTHDR(&msghdr);
+    cmsg->cmsg_len = msghdr.msg_controllen;
+    cmsg->cmsg_level = SOL_SOCKET;
+    cmsg->cmsg_type = SCM_RIGHTS;
+    ((int *)CMSG_DATA(cmsg))[0] = fd;
+
+    return(sendmsg(sock, &msghdr, 0) >= 0 ? 0 : -1);
 }
 
 int sock_client(int fd)
