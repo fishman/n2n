36951fde969c4efdf49ff861924b211f680f4699
Reza Jelveh <toyb0mb@gmx.net>
2009-10-13 13:37:54 +0200

    merge changes from socket test

--- osx/TunHelper.m
+++ osx/TunHelper.m
@@ -10,20 +10,73 @@
 #include <sys/socket.h>
 #include <sys/un.h>
 #include <limits.h>
+#include <signal.h>
 
-#define ADDRESS "/tmp/somesocketstuffs"
+#define ADDRESS "/tmp/n2n"
 #define N2N_OSX_TAPDEVICE_SIZE 32
 
-int send_fd(int sock, int fd)
+#define QLEN 10
+
+#if 1
+/* size of control buffer to send/recv one file descriptor */
+#define CONTROLLEN  CMSG_LEN(sizeof(int))
+
+static struct cmsghdr   *cmptr = NULL;  /* malloc'ed first time */
+
+/*
+ * Pass a file descriptor to another process.
+ * If fd<0, then -fd is sent back instead as the error status.
+ */
+int
+send_fd(int fd, int fd_to_send)
+{
+    struct iovec    iov[1];
+    struct msghdr   msg;
+    char            buf[2]; /* send_fd()/recv_fd() 2-byte protocol */
+
+    iov[0].iov_base = buf;
+    iov[0].iov_len  = 2;
+    msg.msg_iov     = iov;
+    msg.msg_iovlen  = 1;
+    msg.msg_name    = NULL;
+    msg.msg_namelen = 0;
+    if (fd_to_send < 0) {
+        msg.msg_control    = NULL;
+        msg.msg_controllen = 0;
+        buf[1] = -fd_to_send;   /* nonzero status means error */
+        if (buf[1] == 0)
+            buf[1] = 1; /* -256, etc. would screw up protocol */
+    } else {
+        if (cmptr == NULL && (cmptr = malloc(CONTROLLEN)) == NULL)
+            return(-1);
+        cmptr->cmsg_level  = SOL_SOCKET;
+        cmptr->cmsg_type   = SCM_RIGHTS;
+        cmptr->cmsg_len    = CONTROLLEN;
+        msg.msg_control    = cmptr;
+        msg.msg_controllen = CONTROLLEN;
+        *(int *)CMSG_DATA(cmptr) = fd_to_send;     /* the fd to pass */
+        buf[1] = 0;          /* zero status means OK */
+    }
+    buf[0] = 0;              /* null byte flag to recv_fd() */
+    if (sendmsg(fd, &msg, 0) != 2)
+        return(-1);
+    return(0);
+}
+#else
+#define ANCIL_FD_BUFFER(n) \
+    struct { \
+	struct cmsghdr h; \
+	int fd[n]; \
+    }
+
+int
+ancil_send_fds_with_buffer(int sock, const int *fds, unsigned n_fds, void *buffer)
 {
     struct msghdr msghdr;
     char nothing = '!';
     struct iovec nothing_ptr;
     struct cmsghdr *cmsg;
-    struct {
-        struct cmsghdr h;
-        int fd[1];
-    } buffer;
+    int i;
 
     nothing_ptr.iov_base = &nothing;
     nothing_ptr.iov_len = 1;
@@ -32,43 +85,60 @@ int send_fd(int sock, int fd)
     msghdr.msg_iov = &nothing_ptr;
     msghdr.msg_iovlen = 1;
     msghdr.msg_flags = 0;
-    msghdr.msg_control = (void*)&buffer;
-    msghdr.msg_controllen = sizeof(struct cmsghdr) + sizeof(int);
+    msghdr.msg_control = buffer;
+    msghdr.msg_controllen = sizeof(struct cmsghdr) + sizeof(int) * n_fds;
     cmsg = CMSG_FIRSTHDR(&msghdr);
     cmsg->cmsg_len = msghdr.msg_controllen;
     cmsg->cmsg_level = SOL_SOCKET;
     cmsg->cmsg_type = SCM_RIGHTS;
-    ((int *)CMSG_DATA(cmsg))[0] = fd;
-
+    for(i = 0; i < n_fds; i++)
+	((int *)CMSG_DATA(cmsg))[i] = fds[i];
     return(sendmsg(sock, &msghdr, 0) >= 0 ? 0 : -1);
 }
 
-int sock_client(int fd)
+int
+send_fd(int sock, int fd)
 {
-    int s,len;
+    ANCIL_FD_BUFFER(1) buffer;
+
+    return(ancil_send_fds_with_buffer(sock, &fd, 1, &buffer));
+}
+#endif
+int sock_server(int fd_to_send){
+    int fd, cfd;
     struct sockaddr_un saun;
 
-    if((s = socket(PF_LOCAL, SOCK_STREAM, 0)) < 0){
-        perror("client: socket");
+    if((fd = socket(PF_UNIX, SOCK_STREAM, 0)) < 0){
+        perror("server: socket");
+        return -1;
     }
 
-    saun.sun_family = PF_LOCAL;
+    saun.sun_family = AF_UNIX;
     strcpy(saun.sun_path, ADDRESS);
-
-    len = sizeof(saun.sun_family) + strlen(saun.sun_path);
-
-    if (connect(s, (const struct sockaddr *)&saun, len) < 0) {
-        perror("client: connect");
-        exit(1);
-    }
-
-    if (send_fd(s, fd)) {
-        perror("client: send fd");
-        exit(1);
-    }
     unlink(ADDRESS);
 
-    return 0;
+    if (bind(fd, (struct sockaddr *)&saun, sizeof(saun)) < 0) {
+        perror("server: bind");
+        return -2;
+    }
+
+    if (listen(fd, QLEN) < 0) { /* tell kernel we're a server */
+        perror("server: listen");
+        return -3;
+    }
+
+    cfd = accept(fd, NULL, NULL);
+    if (cfd < 0) {
+        perror("server: accept");
+        return -4;
+    }
+
+    if (send_fd(cfd, fd_to_send)) {
+        perror("client: send fd");
+        return -1;
+    }
+
+    return fd;
 }
 
 int main (int argc, const char * argv[]) {
@@ -77,18 +147,17 @@ int main (int argc, const char * argv[]) {
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
 
     NSLog(@"TunHelper started!");
-
     for (i = 0; i < 255; i++) {
         snprintf(tap_device, sizeof(tap_device), "/dev/tap%d", i);
 
         fd = open(tap_device, O_RDWR);
         if(fd > 0) {
-            NSLog(@"Succesfully opened %s", tap_device);
+            NSLog(@"Succesfully opened %s, fd: %d", tap_device, fd);
+            sock_server(fd);
             break;
         }
     }
 
-    sock_client(fd);
     [pool drain];
-    return 0;
+    return i;
 }
